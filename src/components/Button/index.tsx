bbf12400d35161daf2da29b9d3cc9b834a84e127
cullyfung <cullyfung@gamil.com>
2024-01-01 21:01:39 +0800

    chore: add story book

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <button className="text-blue-300">Button</button>;
+import { ComponentProps } from 'react';
+
+type ButtonProps = ComponentProps<'button'>;
+
+const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button
+      className="text-blue-500"
+      {...props}
+    />
+  );
 };
 
 export default Button;
