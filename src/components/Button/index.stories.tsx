8c880ea0349706623716a7e6ad35726989790df7
cullyfung <cullyfung@gamil.com>
2024-01-01 21:36:07 +0800

    feat: add button

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -14,8 +14,29 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
   args: {
-    children: 'Click me'
+    children: 'Click me',
+    variant: 'solid',
+    size: 'md',
+    colorSchema: 'primary'
+  }
+};
+
+export const Outline: Story = {
+  args: {
+    children: 'Button',
+    variant: 'outline',
+    size: 'md',
+    colorSchema: 'primary'
+  }
+};
+
+export const Ghost: Story = {
+  args: {
+    children: 'Button',
+    variant: 'ghost',
+    size: 'md',
+    colorSchema: 'primary'
   }
 };
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,62 @@
-import { ComponentProps } from 'react';
+import { cn } from '@/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentProps, forwardRef } from 'react';
 
-type ButtonProps = ComponentProps<'button'>;
+const buttonStyles = cva(
+  ['w-full', 'rounded-md', 'font-semibold', 'focus:outline-none', 'disabled:cursor-not-allowed'],
+  {
+    variants: {
+      variant: {
+        solid: '',
+        outline: 'border-2',
+        ghost: 'transition-colors duration-300'
+      },
+      size: {
+        sm: 'px-4 py-2 text-sm',
+        md: 'px-6 py-3 text-base',
+        lg: 'px-8 py-4 text-lg'
+      },
+      colorSchema: {
+        primary: 'text-white'
+      }
+    },
+    compoundVariants: [
+      {
+        variant: 'solid',
+        colorSchema: 'primary',
+        className: 'bg-primary-500 hover:bg-primary-600'
+      },
+      {
+        variant: 'outline',
+        colorSchema: 'primary',
+        className: 'text-primary-600 border-primary-500 bg-transparent hover:bg-primary-100'
+      },
+      {
+        variant: 'ghost',
+        colorSchema: 'primary',
+        className: 'text-primary-600 bg-transparent hover:bg-primary-100'
+      }
+    ],
+    defaultVariants: {
+      variant: 'solid',
+      size: 'md',
+      colorSchema: 'primary'
+    }
+  }
+);
 
-const Button = ({ ...props }: ButtonProps) => {
-  return (
-    <button
-      className="text-blue-500"
-      {...props}
-    />
-  );
-};
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof buttonStyles>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, size, colorSchema, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(buttonStyles({ variant, size, colorSchema, className }))}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Button;
